79b3d87aef0f8361959efa96ca910f5078f3b727
gneyhabub <gneyhabub@stride.one>
2020-08-24 23:52:03 +0300

    first version of lexer

--- main.dart
+++ main.dart
@@ -1,3 +1,20 @@
+import 'dart:async';
+import 'dart:io';
+
 void main() {
-  print('Hello, World!');
+  void lexer(path) {
+    String fileString;
+    new File(path).readAsString().then((String contents) {
+      fileString = contents;
+      RegExp exp = new RegExp(
+          "([a-zA-Z_]\\w*|[0-9.]|[:;(),\\[\\]=.<>\/*%+\n-])",
+          multiLine: true);
+      Iterable<RegExpMatch> matches = exp.allMatches(fileString);
+      matches.forEach((element) {
+        print(fileString.substring(element.start, element.end));
+      });
+    });
+  }
+
+  lexer('./tests/bubble_sort.isc');
 }
