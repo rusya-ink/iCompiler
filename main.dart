4658ef218e9529cea4fd9db3b548c9d030a38868
gneyhabub <gneyhabub@stride.one>
2020-08-25 18:15:41 +0300

    lexer version 2

--- main.dart
+++ main.dart
@@ -1,20 +1,72 @@
 import 'dart:async';
 import 'dart:io';
 
+class Token {
+  String value;
+  int start;
+  int end;
+  int type = null;
+  bool isFaulty;
+
+  Token(String value, int start, int end, bool isFaulty) {
+    this.value = value;
+    this.start = start;
+    this.end = end;
+    this.isFaulty = isFaulty;
+  }
+}
+
 void main() {
-  void lexer(path) {
+  List<Token> lexer(path) {
     String fileString;
+    var tokens = [];
     new File(path).readAsString().then((String contents) {
       fileString = contents;
       RegExp exp = new RegExp(
           "([a-zA-Z_]\\w*|[0-9.]|[:;(),\\[\\]=.<>\/*%+\n-])",
           multiLine: true);
-      Iterable<RegExpMatch> matches = exp.allMatches(fileString);
-      matches.forEach((element) {
-        print(fileString.substring(element.start, element.end));
-      });
+      var matches = exp.allMatches(fileString).toList();
+
+      for (var i = 0; i < matches.length; i++) {
+        if (i > 0) {
+          //Get tokens "in between"
+          if (matches[i].start - matches[i - 1].end > 0) {
+            tokens.add(new Token(
+                fileString.substring(matches[i - 1].end, matches[i].start),
+                matches[i].start,
+                matches[i - 1].end,
+                true));
+          }
+        }
+        //Get correct Tokens
+        tokens.add(new Token(
+            fileString.substring(matches[i].start, matches[i].end),
+            matches[i].start,
+            matches[i].end,
+            false));
+      }
+      /** Function that says if the given string has any character distinct from the whitespace */
+      bool containsOnlySpaces(String str) {
+        var arr = str.codeUnits;
+        for (var i = 0; i < arr.length; i++) {
+          if (arr[i] != 32) {
+            return false;
+          }
+        }
+        return true;
+      }
+
+      for (var i = 0; i < tokens.length; i++) {
+        // Remove spaces from token list
+        if (tokens[i].isFaulty && containsOnlySpaces(tokens[i].value))
+          tokens.removeAt(i);
+      }
+      return tokens;
     });
   }
 
-  lexer('./tests/bubble_sort.isc');
+  List<Token> tokensList = lexer('./tests/bubble_sort.isc');
+  tokensList.forEach((element) {
+    print(element.value);
+  });
 }
