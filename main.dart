2086f3ab6e4435389756580c9ba4eeb5b323291d
Lev Chelyadinov <leva181777@gmail.com>
2020-09-04 18:16:20 +0300

    Transform the lexer into a generator, fix the regex

--- main.dart
+++ main.dart
@@ -4,49 +4,45 @@ class Token {
   final String value;
   final int start;
   final int end;
-  final int type = null;
   final bool isFaulty;
-  String toString(){
+
+  String toString() {
     return 'Token "${value == '\n' ? '\\n' : value}", ${start}–${end}';
   }
+
   const Token(this.value, this.start, this.end, {this.isFaulty});
 }
 
-RegExp langTokenPtn = new RegExp(
-  "([a-zA-Z_]\\w*|[0-9.]|[:;(),\\[\\]=.<>\/*%+\n-])",
+RegExp langTokenPtn = RegExp(
+  "([a-zA-Z_]\\w*|[0-9]+(?:\\.[0-9]*)?|[:;(),\\[\\]=.<>\/*%+\n-])",
   multiLine: true,
 );
+RegExp allWhitespacePtn = RegExp("^\\s+\$");
 
-List<Token> lexer(path){
-  var tokens = <Token>[];
-  var fileContents = new File(path).readAsStringSync();
-  Iterable<RegExpMatch> iterator = langTokenPtn.allMatches(fileContents);
-
+Iterable<Token> lexer(String sourceCode) sync* {
   RegExpMatch previousMatch = null;
-  for (RegExpMatch match in iterator) {
+  for (var match in langTokenPtn.allMatches(sourceCode)) {
     if (previousMatch != null && previousMatch.end != match.start) {
-      tokens.add(new Token(
-        fileContents.substring(previousMatch.end, match.start),
+      var tokenValue = sourceCode.substring(previousMatch.end, match.start);
+      yield Token(
+        tokenValue,
         previousMatch.end,
         match.start,
-        isFaulty: true,
-      ));
+        isFaulty: !allWhitespacePtn.hasMatch(tokenValue),
+      );
     }
-    tokens.add(new Token(
+
+    yield Token(
       match.group(1),
       match.start,
       match.end,
       isFaulty: false,
-    ));
+    );
+
     previousMatch = match;
   }
-
-  return tokens;
 }
 
-void main(){
-  List<Token> tokensList = lexer('./tests/bubble_sort.isc');
-  tokensList.forEach((element) {
-    print(element);
-  });
+void main() {
+  lexer(File('./tests/bubble_sort.isc').readAsStringSync()).forEach(print);
 }
