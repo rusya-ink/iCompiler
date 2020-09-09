cfc160e331f8e290b948f4788ff3bc74083dcac8
rusya-ink <43722585+rusya-ink@users.noreply.github.com>
2020-09-09 18:08:22 +0300

    Update range.dart

--- ast-nodes/range.dart
+++ ast-nodes/range.dart
@@ -1,6 +1,8 @@
+import '../syntax-error.dart';
 import 'node.dart';
 import 'expression.dart';
 import '../print-utils.dart';
+import '../lexer.dart';
 
 /// An iteration range for the `for` loop.
 class Range implements Node {
@@ -9,7 +11,30 @@ class Range implements Node {
 
   Range(this.start, this.end);
 
-  // TODO: implement .parse()
+  factory Range.parse(Iterable<Token> tokens) {
+
+    var iterator = tokens.iterator;
+    if(!iterator.moveNext()){
+      throw SyntaxError(iterator.current, "Expected '..' in range");
+    }
+
+    var expStart = Expression.parse(
+        consumeUntil(iterator, RegExp("\^"+RegExp.escape("..")+"\$"))
+    );
+    
+    var expEnd;
+    if(iterator.current==".."){
+      if(iterator.moveNext()){
+        expEnd = Expression.parse(consumeFull(iterator));
+      }else{
+        expEnd = Expression.parse([]);
+      }
+    }else{
+      throw SyntaxError(iterator.current, "Expected '..' in range");
+    }
+
+    return Range(expStart, expEnd);
+  }
 
   String toString({int depth = 0, String prefix = ''}) {
     return (
