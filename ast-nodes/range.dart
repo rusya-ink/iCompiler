3af5564592f5a2b4336a0b852429f7dca9871bdd
rusya-ink <43722585+rusya-ink@users.noreply.github.com>
2020-09-10 17:30:38 +0300

    Update range.dart

--- ast-nodes/range.dart
+++ ast-nodes/range.dart
@@ -12,24 +12,22 @@ class Range implements Node {
   Range(this.start, this.end);
 
   factory Range.parse(Iterable<Token> tokens) {
-
     var iterator = tokens.iterator;
-    if(!iterator.moveNext()){
+    if (!iterator.moveNext()) {
       throw SyntaxError(iterator.current, "Expected '..' in range");
     }
 
     var expStart = Expression.parse(
-        consumeUntil(iterator, RegExp("\^"+RegExp.escape("..")+"\$"))
-    );
-    
+        consumeUntil(iterator, RegExp("\^" + RegExp.escape("..") + "\$")));
+
     var expEnd;
-    if(iterator.current==".."){
-      if(iterator.moveNext()){
+    if (iterator.current == "..") {
+      if (iterator.moveNext()) {
         expEnd = Expression.parse(consumeFull(iterator));
-      }else{
+      } else {
         expEnd = Expression.parse([]);
       }
-    }else{
+    } else {
       throw SyntaxError(iterator.current, "Expected '..' in range");
     }
 
@@ -37,10 +35,8 @@ class Range implements Node {
   }
 
   String toString({int depth = 0, String prefix = ''}) {
-    return (
-      drawDepth('${prefix}Range', depth)
-      + (this.start?.toString(depth: depth + 1, prefix: 'start: ') ?? '')
-      + (this.end?.toString(depth: depth + 1, prefix: 'end: ') ?? '')
-    );
+    return (drawDepth('${prefix}Range', depth) +
+        (this.start?.toString(depth: depth + 1, prefix: 'start: ') ?? '') +
+        (this.end?.toString(depth: depth + 1, prefix: 'end: ') ?? ''));
   }
 }
