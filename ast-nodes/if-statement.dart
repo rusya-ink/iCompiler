a4b5816bac288efe3e12ca88276e02019ac0c179
CeeVarouqa <ceevarouqa@gmail.com>
2020-09-11 01:01:14 +0300

    Update if-statement.dart

--- ast-nodes/if-statement.dart
+++ ast-nodes/if-statement.dart
@@ -12,6 +12,32 @@ class IfStatement implements Statement {
 
   // TODO: implement .parse()
 
+  factory IfStatement.parse(Iterable<Token> tokens) {
+    var iterator = tokens.iterator;
+    checkNext(iterator, RegExp('if\$'), "Expected 'if'");
+    var expressionBody = consumeUntil(iterator, RegExp("^then\$"));
+    checkNext(iterator, RegExp('then\$'), "Expected 'then'");
+    var body = consumeUntil(iterator, RegExp("^else\$"));
+    checkNext(iterator, RegExp('else\$'), "Expected 'else'");
+    var elseBody = consumeUntil(iterator, RegExp("^end\$"));
+    checkThis(iterator, RegExp('end\$'), "Expected 'end'");
+    checkNoMore(iterator);
+
+    if (expressionBody.isEmpty) {
+      throw SyntaxError(iterator.current, "Expected a condition");
+    }
+
+    if (body.isEmpty) {
+      throw SyntaxError(iterator.current, "Expected a true statement");
+    }
+
+    var expBody = Expression.parse(expressionBody);
+    var trueBlock = List<Statement>.parse(body);
+    var falseBlock = List<Statement>.parse(elseBody);
+
+    return IfStatement(expBody, trueBlock, falseBlock);
+  }
+
   String toString({int depth = 0, String prefix = ''}) {
     return (
       drawDepth('${prefix}IfStatement', depth)
