869a7073d0ea09e8642afc3f0edac1f121d6670f
mari1647iv <ima1647@outlook.com>
2020-09-11 02:48:03 +0300

    Fix assignment parser

--- ast-nodes/assignment.dart
+++ ast-nodes/assignment.dart
@@ -2,6 +2,11 @@ import 'statement.dart';
 import 'modifiable-primary.dart';
 import 'expression.dart';
 import '../print-utils.dart';
+import '../lexer.dart';
+import '../iterator-utils.dart';
+import '../syntax-error.dart';
+
+
 
 /// An assignment of the value on the right hand side ([rhs]) to the left hand side ([lhs]).
 class Assignment implements Statement {
@@ -12,10 +17,19 @@ class Assignment implements Statement {
 
   factory Assignment.parse(Iterable<Token> tokens) {
     var iter = tokens.iterator;
+
+    if (tokens.isEmpty) {
+      throw SyntaxError(iterator.current, "Expected an assignment");
+    }
+
     var tempLhs = consumeUntil(iter, RegExp("^:=\$"));
+
+    if (tempLhs.isEmpty) {
+      throw SyntaxError(iterator.current, "Expected an identifier");
+    }
+
     checkNext(iter, RegExp(':=\$'), 'Expected ":="');
-    var expressionBody = consumeUntil(iterator, RegExp("^[;\n]\$"));
-    checkNoMore(iterator);
+    iter.moveNext();
     return Assignment(ModifiablePrimary.parse(tempLhs), Expression.parse(consumeFull(iter)));
   }
 
