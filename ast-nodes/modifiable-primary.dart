e784f3e67ad83556ffa5065f18d1a713317119af
rusya-ink <43722585+rusya-ink@users.noreply.github.com>
2020-09-10 19:52:55 +0300

    Update modifiable-primary.dart

--- ast-nodes/modifiable-primary.dart
+++ ast-nodes/modifiable-primary.dart
@@ -13,19 +13,17 @@ import 'expression.dart';
 abstract class ModifiablePrimary implements Primary {
   factory ModifiablePrimary.parse(Iterable<Token> tokens) {
     var iterator = tokens.iterator;
-    var result;
-    checkNext(iterator, RegExp('[a-zA-Z_]\w*\$'), "Expected identifier");
+    ModifiablePrimary result;
+    checkNext(iterator, RegExp('[a-zA-Z_]\\w*\$'), "Expected identifier");
     result = Variable(iterator.current.value);
 
     while (iterator.moveNext()) {
       if (iterator.current.value == ".") {
-        checkNext(iterator, RegExp('[a-zA-Z_]\w*\$'), "Expected identifier");
+        checkNext(iterator, RegExp('[a-zA-Z_]\\w*\$'), "Expected identifier");
         result = FieldAccess(iterator.current.value, result);
       } else if (iterator.current.value == "[") {
-        var exp = Expression.parse(consumeStackUntil(
-            iterator,
-            RegExp("\^" + RegExp.escape("[") + "\$"),
-            RegExp("\^" + RegExp.escape("]") + "\$")));
+        var exp = Expression.parse(
+            consumeStackUntil(iterator, RegExp('\\[\$'), RegExp('\\]\$')));
         result = IndexAccess(exp, result);
       } else {
         throw SyntaxError(iterator.current, "Unexpected token");
