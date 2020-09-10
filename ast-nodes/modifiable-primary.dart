0b553dd40045f5b07ba5e8d8154dbfb77926dd94
rusya-ink <43722585+rusya-ink@users.noreply.github.com>
2020-09-10 17:26:40 +0300

    Update modifiable-primary.dart

--- ast-nodes/modifiable-primary.dart
+++ ast-nodes/modifiable-primary.dart
@@ -5,6 +5,7 @@ import 'field-access.dart';
 import 'variable.dart';
 import 'index-access.dart';
 import '../syntax-error.dart';
+import 'expression.dart';
 
 /// An abstract writable entity – something that can appear in the LHS of the assignment.
 ///
@@ -16,15 +17,17 @@ abstract class ModifiablePrimary implements Primary {
     checkNext(iterator, RegExp('[a-zA-Z_]\w*\$'), "Expected identifier");
     result = Variable(iterator.current.value);
 
-    while(iterator.moveNext()){
-      if(iterator.current.value=="."){
+    while (iterator.moveNext()) {
+      if (iterator.current.value == ".") {
         checkNext(iterator, RegExp('[a-zA-Z_]\w*\$'), "Expected identifier");
         result = FieldAccess(iterator.current.value, result);
-      }else if(iterator.current.value=="["){
-        var exp = consumeStackUntil(iterator, RegExp("\^"+RegExp.escape("[")+"\$"),
-            RegExp("\^"+RegExp.escape("]")+"\$"));
+      } else if (iterator.current.value == "[") {
+        var exp = Expression.parse(consumeStackUntil(
+            iterator,
+            RegExp("\^" + RegExp.escape("[") + "\$"),
+            RegExp("\^" + RegExp.escape("]") + "\$")));
         result = IndexAccess(exp, result);
-      }else{
+      } else {
         throw SyntaxError(iterator.current, "Unexpected token");
       }
     }
