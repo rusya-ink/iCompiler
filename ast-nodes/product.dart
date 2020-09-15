84680bc6dc0d1a2deba22ff6c33976f71acc5b5c
CeeVarouqa <ceevarouqa@gmail.com>
2020-09-15 18:25:30 +0300

    product and some changes to main

--- ast-nodes/product.dart
+++ ast-nodes/product.dart
@@ -1,9 +1,22 @@
 import 'sum.dart';
 import '../lexer.dart';
+import 'expression.dart';
 
 /// An abstract multiplying operator.
 abstract class Product implements Sum {
   factory Product.parse(Iterable<Token> tokens) {
+    final iter = tokens.iterator;
+    List<Token> expr = consumeUntil(iter, RegExp('[*/%]\$'));
+    var wholeExpression = Expression.parsePrioritized(expr);
 
+    if (iter.current?.value == '*') {
+      iter.moveNext();
+      return MulOperator(Product.parse(product), Sum.parse(consumeFull(iter)));
+    } else if (iter.current?.value == '/') {
+      iter.moveNext();
+      return DivOperator(Product.parse(product), Sum.parse(consumeFull(iter)));
+    } else {
+      return Product.parse(product);
+    }
   }
 }
--- main.dart
+++ main.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 import 'lexer.dart';
 import 'syntax-error.dart';
+import 'ast-nodes/index.dart';
 
 void main() {
   try {
-    splitToTokens(File('./tests/bubble_sort.isc').readAsStringSync()).forEach(print);
+    Program.parse(splitToTokens(File('./tests/bubble_sort.isc').readAsStringSync()));
   } on SyntaxError catch (e) {
     print(e);
   }
