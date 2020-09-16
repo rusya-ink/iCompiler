0be21c1ad6ef22aa9d291b687b5a0ea5ce00574f
Lev Chelyadinov <leva181777@gmail.com>
2020-09-16 14:00:51 +0300

    Implement the array type parsing

--- ast-nodes/array-type.dart
+++ ast-nodes/array-type.dart
@@ -1,18 +1,37 @@
 import 'var-type.dart';
+import 'expression.dart';
 import '../print-utils.dart';
+import '../iterator-utils.dart';
+import '../lexer.dart';
 
 /// An array type with optional [size].
 class ArrayType implements VarType {
-  int size;
+  Expression size;
   VarType elementType;
 
   ArrayType(this.size, this.elementType);
 
-  // TODO: implement .parse()
+  factory ArrayType.parse(Iterable<Token> tokens) {
+    var iterator = tokens.iterator;
+    checkNext(iterator, RegExp('array\$'), "Expected 'array'");
+    checkNext(iterator, RegExp('\\[\$'), "Expected '['");
+    iterator.moveNext();
+    var sizeTokens = consumeUntil(iterator, RegExp("\\]\$"));
+    checkThis(iterator, RegExp('\\]\$'), "Expected ']'");
+    var type = VarType.parse(consumeFull(iterator));
+
+    Expression size = null;
+    if (!sizeTokens.isEmpty) {
+      size = Expression.parse(sizeTokens);
+    }
+
+    return ArrayType(size, type);
+  }
 
   String toString({int depth = 0, String prefix = ''}) {
     return (
-      drawDepth('${prefix}ArrayType(size = ${this.size})', depth)
+      drawDepth('${prefix}ArrayType', depth)
+      + (this.size?.toString(depth: depth + 1, prefix: 'size: ') ?? '')
       + (this.elementType?.toString(depth: depth + 1, prefix: 'element type: ') ?? '')
     );
   }
