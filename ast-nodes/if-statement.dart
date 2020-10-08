921c4ead3059ff31fbbd8f86b9f832e9d6662dd5
mari1647iv <ima1647@outlook.com>
2020-10-08 16:03:58 +0300

    Fix import paths

--- ast-nodes/if-statement.dart
+++ ast-nodes/if-statement.dart
@@ -8,8 +8,8 @@ import '../lexer.dart';
 import '../syntax-error.dart';
 import '../symbol-table/scope.dart';
 import '../symbol-table/scope-element.dart';
-import '../types/boolean-type.dart';
-import '../../semantic-utils.dart';
+import 'types/boolean-type.dart';
+import '../semantic-utils.dart';
 
 /// A conditional statement.
 class IfStatement implements Statement, ScopeCreator {
